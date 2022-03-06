8d7369be810f7aa89616cdb0b6512261bf643603
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2022-03-06 18:50:53 +0100

    Add second planet

--- WpfPlanetSimulation/ViewModel/MainViewModel.cs
+++ WpfPlanetSimulation/ViewModel/MainViewModel.cs
@@ -6,6 +6,9 @@ namespace WpfPlanetSimulation.ViewModel
 {
   public class MainViewModel : ViewModelBase
   {
+    const double degreesChangeMars = 365.0 / 687.0;
+    private bool _isAnimated;
+
     public MainViewModel()
     {
       CompositionTarget.Rendering += CompositionTarget_Rendering;
@@ -15,19 +18,22 @@ namespace WpfPlanetSimulation.ViewModel
     {
       if (IsAnimated)
       {
-        PositionMars.Degrees++;
+        PositionMars.Degrees += degreesChangeMars;
+        PositionEarth.Degrees += 1;
       }
     }
 
-    public PositionViewModel PositionMars { get; } = new();
-
-    private bool _isAnimated;
+    public PositionViewModel PositionMars { get; } = new() { CenterX = 200, CenterY = 200 };
+    public PositionViewModel PositionEarth { get; } = new() { CenterX = 200, CenterY = 200, Radius = 100 };
 
     public bool IsAnimated
     {
-      get { return _isAnimated; }
-      set { _isAnimated = value; }
+      get => _isAnimated;
+      set
+      {
+        _isAnimated = value;
+        RaisePropertyChanged(nameof(IsAnimated));
+      }
     }
-
   }
 }
--- WpfPlanetSimulation/ViewModel/PositionViewModel.cs
+++ WpfPlanetSimulation/ViewModel/PositionViewModel.cs
@@ -4,17 +4,40 @@ namespace WpfPlanetSimulation.ViewModel
 {
   public class PositionViewModel : ViewModelBase
   {
-    private int _degrees;
+    private double _degrees;
     private double _radius = 150;
+    private double _centerX;
+    private double _centerY;
 
     public PositionViewModel()
     {
       CalculateXAndY();
     }
-    
 
     public double X { get; private set; }
     public double Y { get; private set; }
+
+    public double CenterX
+    {
+      get => _centerX;
+      set
+      {
+        _centerX = value;
+        CalculateXAndY();
+        RaisePropertyChanged(nameof(CenterX));
+      }
+    }
+    
+    public double CenterY
+    {
+      get => _centerY;
+      set
+      {
+        _centerY = value;
+        CalculateXAndY();
+        RaisePropertyChanged(nameof(CenterY));
+      }
+    }
 
     public double Radius
     {
@@ -27,16 +50,12 @@ namespace WpfPlanetSimulation.ViewModel
       }
     }
 
-    public int Degrees
+    public double Degrees
     {
       get => _degrees;
       set
       {
         _degrees = value;
-        if (_degrees > 360)
-        {
-          _degrees = 0;
-        }
         CalculateXAndY();
         RaisePropertyChanged(nameof(Degrees));
       }
@@ -46,8 +65,8 @@ namespace WpfPlanetSimulation.ViewModel
     {
       var angle = (Math.PI / 180) * Degrees;
 
-      X = Radius * Math.Cos(angle) + Radius;
-      Y = Radius * Math.Sin(angle) + Radius;
+      X = Radius * Math.Cos(angle) + CenterX;
+      Y = Radius * Math.Sin(angle) + CenterY;
       RaisePropertyChanged(nameof(X));
       RaisePropertyChanged(nameof(Y));
     }
